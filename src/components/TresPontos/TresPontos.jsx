fd510fdb60e959fef9d80687997ca2ab507ece5a
Julia <juliasoaresdealmeidaj@gmail.com>
2023-09-28 10:56:14 -0300

    Arrumando toggle button

--- src/components/TresPontos/TresPontos.jsx
+++ src/components/TresPontos/TresPontos.jsx
@@ -2,35 +2,34 @@
 import React, { useState } from "react";
 
 //Import css, img e components
-import {ModalDelete} from "../ModalDelete/ModalDelete"
+import { ModalDelete } from "../ModalDelete/ModalDelete"
 import tresPontos from "./img/tres-pontos.png"
 import "./TresPontos.css"
 
 export function TresPontos() {
-    const [mostrarLista, setMostrarLista] = useState(false);
-
-    const toggleLista = () => {
-        setMostrarLista(!mostrarLista);
-    };
-
-    const handleMouseLeave = () => {
-        setTimeout(() => { setMostrarLista(false); }, 3000);
-    };
-
     //Modal para deletar um item
     const [openModalDelete, setOpenModalDelete] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
-        <div className="container-tres-pontos" onMouseLeave={handleMouseLeave}>
-            <button className="botao" onClick={toggleLista}>
-                <img src={tresPontos} alt="Três pontoa" className="tres-pontos" />
+        <div className={`container-tres-pontos ${menuOpen ? 'open' : ''}`}>
+
+            <button onClick={toggleMenu} className="toggle-button">
+                <img src={tresPontos} alt="Três pontos" className="tres-pontos" />
             </button>
-            {mostrarLista && (
+
+            {menuOpen && (
+
                 <ul className="lista">
                     <li>Editar</li>
                     <li onClick={() => setOpenModalDelete(true)}>Excluir</li>
                     <ModalDelete isOpenModal={openModalDelete} setModalOpenDelete={() => setOpenModalDelete(!openModalDelete)}></ModalDelete>
                 </ul>
+
             )}
         </div>
     );
