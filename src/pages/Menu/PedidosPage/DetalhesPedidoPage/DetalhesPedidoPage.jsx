d8c098c93d37d46207b6df63bae8a14910b74781
OswaldBarbosa <oswaldo.barbosacruz1902@gmail.com>
2023-10-19 15:43:31 -0300

    detalhes pedido quase finalizada

--- src/pages/Menu/PedidosPage/DetalhesPedidoPage/DetalhesPedidoPage.jsx
+++ src/pages/Menu/PedidosPage/DetalhesPedidoPage/DetalhesPedidoPage.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import "./DetalhesPedidoPage.css"
 import { MenuNavigation } from "../../../../components/MenuNavigation/MenuNavigation";
 import { HeaderDetalhesPedido } from "../../../../components/HeaderDetalhesPedido/HeaderDetalhesPedido";
+import { StatusPedido } from "../../../../components/StatusPedido/StatusPedido";
 
 export function DetalhesPedidoPage() {
     return (
@@ -22,18 +23,7 @@ export function DetalhesPedidoPage() {
 
                     <HeaderDetalhesPedido></HeaderDetalhesPedido>
 
-                    <div className="container-status-pedido">
-
-                        <div className="title-status-pedido">
-
-                            <span className="text-status-detalhes-pedido">Status</span>
-
-                            <span className="text-despachado">Despachado</span>
-
-                        </div>
-
-
-                    </div>
+                    <StatusPedido></StatusPedido>
 
                 </div>
 
