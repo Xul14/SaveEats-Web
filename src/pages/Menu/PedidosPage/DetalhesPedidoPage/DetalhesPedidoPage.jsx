75d8ef6676ef0b6e2b744bf2d150b203ebdc48d7
OswaldBarbosa <oswaldo.barbosacruz1902@gmail.com>
2023-10-20 16:55:11 -0300

    tela quase finalizada

--- src/pages/Menu/PedidosPage/DetalhesPedidoPage/DetalhesPedidoPage.jsx
+++ src/pages/Menu/PedidosPage/DetalhesPedidoPage/DetalhesPedidoPage.jsx
@@ -6,6 +6,7 @@ import "./DetalhesPedidoPage.css"
 import { MenuNavigation } from "../../../../components/MenuNavigation/MenuNavigation";
 import { HeaderDetalhesPedido } from "../../../../components/HeaderDetalhesPedido/HeaderDetalhesPedido";
 import { StatusPedido } from "../../../../components/StatusPedido/StatusPedido";
+import produtos from "../DetalhesPedidoPage/img/coxinha.jpg"
 
 export function DetalhesPedidoPage() {
     return (
@@ -24,6 +25,35 @@ export function DetalhesPedidoPage() {
                     <HeaderDetalhesPedido></HeaderDetalhesPedido>
 
                     <StatusPedido></StatusPedido>
+
+                    <div className="container-detalhes-pedido-total-pagamento">
+
+                        <span className="title-despachado">Despachado</span>
+
+                        <div className="container-total-pedido">
+
+                            <span className="text-total-pedido">Total</span>
+
+                            <span className="text-preco-total">R$ 65,70</span>
+
+                        </div>
+
+                        <div className="container-pagamento-entrega">
+
+                            <span className="text-pagamento-entrega">Pagamento na entrega</span>
+
+                            <span className="text-forma-pagamento-entrega">Débito Mastercard</span>
+
+                        </div>
+
+                    </div>
+
+                    <div className="container-detalhes-produtos">
+
+                        <img src={produtos} alt="Produtos" className="quantidade-produto" />
+
+                    </div>
+
 
                 </div>
 
