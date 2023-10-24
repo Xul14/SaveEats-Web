56a1afd66196cbb10e192224a8341335db8ff2f0
DWOVÂNNA SANTOS NASCIMENTO <22282185@dsn127m1241955.cfp127aluno.local>
2023-10-24 16:21:51 -0300

    tela perfil pronta

--- src/pages/Menu/PerfilPage/PerfilPage.jsx
+++ src/pages/Menu/PerfilPage/PerfilPage.jsx
@@ -4,6 +4,7 @@ import React from "react";
 //Import css e components
 import "./PerfilPage.css"
 import { MenuNavigation } from "../../../components/MenuNavigation/MenuNavigation";
+import img from './img/padaria.png'
 
 export function PerfilPage() {
     return (
@@ -15,6 +16,80 @@ export function PerfilPage() {
                 </div>
 
                 <div className="container-rigth-perfil">
+
+                    <main>
+
+                    <div class="circle">
+                
+                        <img src={img} alt="" />
+                
+                    </div>
+
+                    <h1 className="titulo">PADARIA DOIS IRMÃOS</h1>
+
+                   <div> 
+
+                     <p className="info1">Nome da loja</p>
+
+                    <input type="text" className="box1"></input>
+
+                   </div>
+
+                    <div>
+
+                    <p className="info2">Telefone</p>
+
+                     <input type="text" className="box2"></input>
+
+                    </div>
+                    
+
+                    <div>
+
+                    <p className="info2">Categoria</p>
+
+                    <input type="text" className="box3"></input>
+
+                    </div>
+
+                    <div>
+
+                    <p className="info4">Nome da loja</p>
+
+                    <input type="text" className="box4"></input>
+
+                    </div>
+                    
+
+                    <div>
+
+                    <p className="info5">Telefone</p>
+
+                    <input type="text" className="box5"></input>
+
+                    </div>
+                    
+                    <div class="botao">
+
+                        <p>Salvar alterações</p>
+
+
+                    </div>
+
+                    </main>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                 </div>
             </div>
