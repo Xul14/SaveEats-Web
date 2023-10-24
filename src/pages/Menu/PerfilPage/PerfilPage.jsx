2180ba4005f745c1d0146f20780033de200c6a3a
JULIA SOARES DE ALMEIDA <22282188@dsn127m1242315.cfp127aluno.local>
2023-10-24 13:21:36 -0300

    Preparando tela de perfil

--- src/pages/Menu/PerfilPage/PerfilPage.jsx
+++ src/pages/Menu/PerfilPage/PerfilPage.jsx
@@ -3,9 +3,21 @@ import React from "react";
 
 //Import css e components
 import "./PerfilPage.css"
+import { MenuNavigation } from "../../../components/MenuNavigation/MenuNavigation";
 
-export function PerfilPage(){
-    return(
-        <p className="p-teste">PERFIL</p>
+export function PerfilPage() {
+    return (
+        <div>
+            <div className="container-perfil">
+
+                <div className="container-left-perfil">
+                    <MenuNavigation></MenuNavigation>
+                </div>
+
+                <div className="container-rigth-perfil">
+
+                </div>
+            </div>
+        </div>
     )
 }
