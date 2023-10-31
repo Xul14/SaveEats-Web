e8267d4f95dd600dbe3be0d05f969f4cf98f4b15
JULIA SOARES DE ALMEIDA <22282188@dsn127m1242315.cfp127aluno.local>
2023-10-31 16:45:00 -0300

    Iniciando tela de perfil

--- src/components/CalendarFuncionamento/CalendarFuncionamento.jsx
+++ src/components/CalendarFuncionamento/CalendarFuncionamento.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
+import 'dayjs/locale/pt-br';
 import dayLocaleData from 'dayjs/plugin/localeData';
 import { Calendar, Col, Radio, Row, Select, Typography, theme } from 'antd';
 
@@ -9,6 +10,7 @@ import { Calendar, Col, Radio, Row, Select, Typography, theme } from 'antd';
 import './CalendarFuncionamento.css'
 
 dayjs.extend(dayLocaleData);
+dayjs.locale('pt-br');
 
 export const CalendarFuncionamento = () => {
 
@@ -23,15 +25,22 @@ export const CalendarFuncionamento = () => {
         borderRadius: token.borderRadiusLG,
         boxShadow: '3px 3px 15px #AFACAB',
     };
+
+    const currentMonthName = dayjs().format('MMMM');
+    // const weekdaysMin = dayjs().localeData().weekdaysMin();
+
     return (
         <div style={wrapperStyle}>
             <Calendar
                 fullscreen={false}
                 headerRender={({ value, type, onChange, onTypeChange }) => {
+
                     const start = 0;
                     const end = 12;
                     const monthOptions = [];
+
                     let current = value.clone();
+
                     const localeData = value.localeData();
                     const months = [];
 
@@ -60,51 +69,10 @@ export const CalendarFuncionamento = () => {
                     }
 
                     return (
-                        <div
-                            style={{
-                                padding: 8,
-                            }}
-                        >
-                            <Typography.Title level={4}>Custom header</Typography.Title>
-                            <Row gutter={8}>
-                                <Col>
-                                    <Radio.Group
-                                        size="small"
-                                        onChange={(e) => onTypeChange(e.target.value)}
-                                        value={type}
-                                    >
-                                        <Radio.Button value="month">Month</Radio.Button>
-                                        <Radio.Button value="year">Year</Radio.Button>
-                                    </Radio.Group>
-                                </Col>
-                                <Col>
-                                    <Select
-                                        size="small"
-                                        dropdownMatchSelectWidth={false}
-                                        className="my-year-select"
-                                        value={year}
-                                        onChange={(newYear) => {
-                                            const now = value.clone().year(newYear);
-                                            onChange(now);
-                                        }}
-                                    >
-                                        {options}
-                                    </Select>
-                                </Col>
-                                <Col>
-                                    <Select
-                                        size="small"
-                                        dropdownMatchSelectWidth={false}
-                                        value={month}
-                                        onChange={(newMonth) => {
-                                            const now = value.clone().month(newMonth);
-                                            onChange(now);
-                                        }}
-                                    >
-                                        {monthOptions}
-                                    </Select>
-                                </Col>
-                            </Row>
+                        <div style={{ padding: 8, }}>
+
+                            <Typography.Title className='title-mounth' level={4}>{currentMonthName}</Typography.Title>
+
                         </div>
                     );
                 }}
--- src/pages/Menu/PerfilPage/PerfilPage.jsx
+++ src/pages/Menu/PerfilPage/PerfilPage.jsx
@@ -1,11 +1,34 @@
 //Import React
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+//Import Axios para integração
+import axios from 'axios'
 
 //Import css e components
 import "./PerfilPage.css"
+import img from './img/padaria.png'
 import { MenuNavigation } from "../../../components/MenuNavigation/MenuNavigation";
 
 export function PerfilPage() {
+
+    //Pegando dados do restaurante
+    const nomeRestaurante = localStorage.getItem("nome_fantasia");
+    const idRestaurante = localStorage.getItem("id");
+    const [dadosPerfil, setDadosPerfil] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`http://localhost:3000/v1/saveeats/restaurante/id/${idRestaurante}`);
+                const responseData = response.data.restaurantes
+                setDadosPerfil(responseData)
+            } catch (error) {
+                console.error('Erro ao obter dados da API:', error)
+            }
+        }
+        fetchData()
+    }, [])
+
     return (
         <div>
             <div className="container-perfil">
@@ -15,6 +38,52 @@ export function PerfilPage() {
                 </div>
 
                 <div className="container-rigth-perfil">
+
+                    <div>
+                        <img src={dadosPerfil[0].foto} alt="" className="foto-restaurante" />
+                    </div>
+
+                    <div>
+                        <h1 className="nome-restaurante">{nomeRestaurante}</h1>
+                    </div>
+
+                    <div className="container-editar-dados">
+
+                        <div className="input-editar-dados">
+                            <p className="nome-input-editar">Nome da estabelecimento</p>
+                            <input type="text" className="input-editar"></input>
+                        </div>
+
+                        <div className="input-editar-dados">
+                            <p className="nome-input-editar">Telefone</p>
+                            <input type="text" className="input-editar"></input>
+                        </div>
+
+                        <div className="input-editar-dados">
+                            <p className="nome-input-editar">Categoria</p>
+                            <input type="text" className="input-editar"></input>
+                        </div>
+
+                        <div className="input-editar-dados">
+                            <p className="nome-input-editar">Nome da loja</p>
+                            <input type="text" className="input-editar"></input>
+                        </div>
+
+                        <div className="input-editar-dados">
+                            <p className="nome-input-editar">Nome da loja</p>
+                            <input type="text" className="input-editar"></input>
+                        </div>
+
+                        <div className="input-editar-dados">
+                            <p className="nome-input-editar">Nome da loja</p>
+                            <input type="text" className="input-editar"></input>
+                        </div>
+
+                    </div>
+
+                    <div className="btn-salvar-edicao">
+                        <button className="btn-salvar-alteracoes">Salvar alterações</button>
+                    </div>
 
                 </div>
             </div>
